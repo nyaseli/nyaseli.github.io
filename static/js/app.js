865fba42ae1d2aa2e03cdeb559311311a4bdd72a
nyaseli <78821876+nyaseli@users.noreply.github.com>
2021-07-17 12:19:32 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -3,6 +3,6 @@
 var testSubjects = d3.select("selDataset");
 d3.json("samples.json").then(function(data){
     data.names.forEach(function(otu){
-    testSubjects.append("option").text(otu).property("value"));
+    testSubjects.append("option").text(otu).property("value");
     });
 })
