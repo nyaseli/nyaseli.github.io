37716c343e36dacaa681bd861ae67bfc700a467b
nyaseli <78821876+nyaseli@users.noreply.github.com>
2021-07-17 13:03:18 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,24 @@
-
-
 var testSubjects = d3.select("#selDataset");
+var dataSet;
 d3.json("samples.json").then((data)=>{
+    dataSet = data;
     data.names.forEach(function(otu){
     testSubjects.append("option").text(otu).property("value");
     });
 })
+
+function displayMetaData(otu, data){
+    var metaData = data.metadata.filter(row => row.id == otu);
+    var metaObject = d3.select("#sample-metadata");
+    metaObject.html(displayMeta(metaData[0]));
+}
+
+function displayMeta(row){
+    var line = "";
+    Object.entries(row).forEach(([k,v])=>{
+        line += `${k}:${v}</br>`;
+    })
+    return line;
+}
+
+displayMetaData(940,dataSet);
