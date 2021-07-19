a7a055916ff27d009057504fff13cbebc76b8dd5
nyaseli <78821876+nyaseli@users.noreply.github.com>
2021-07-19 10:26:05 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -2,11 +2,11 @@ var testSubjects = d3.select("#selDataset");
 var dataSet;
 d3.json("samples.json").then((data)=>{
     dataSet = data;
-    displayMetaData(940,dataSet);
     data.names.forEach(function(otu){
     testSubjects.append("option").text(otu).property("value");
     });
-    
+    displayMetaData(940,dataSet);
+    barChart(940,dataSet);
 })
 
 function displayMetaData(otu, data){
@@ -21,4 +21,30 @@ function displayMeta(row){
         line += `${k}:${v}</br>`;
     })
     return line;
+}
+
+function barChart(otu, data){
+    var chartData = data.samples.filter(sample => sample.id == otu);
+    var mappedData = chartData.map(row => row.otu_ids);
+    var allOtus = [];
+    for(i = 0;i < mappedData[0].length; i++){
+        allOtus.push(`OTU ${mappedData[0][i]}`);
+    }
+    var value = chartData.map(row => row.sample_values);
+    var labels = chartData.map(row => row.otu_labels);
+    var trace = {
+        x:value[0].slice(0,10), 
+        y:allOtus.slice(0,10),
+        text:labels[0].slice(0,10),
+        type:"bar",
+        orientation:"h",
+    };
+    var result = [trace];
+    var layout = {
+        yaxis:{
+            autorange:"reversed"
+        }
+
+    };
+    Plotly.newPlot("bar",result,layout);
 }
